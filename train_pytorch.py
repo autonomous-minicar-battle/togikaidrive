ee29c07d7b50a04292583c7f699b835f35a98296
Romihi <makers.disco@gmail.com>
2024-07-01 01:07:05 +0900

    add denormalize in train

--- train_pytorch.py
+++ train_pytorch.py
@@ -52,12 +52,29 @@ def load_data():
     x = df.iloc[:, 3:]
     y = df.iloc[:, 1:3]
     x_tensor = torch.tensor(x.values, dtype=torch.float32)
-    x_tensor =x_tensor / 2000 # 2000mmを1として正規化
+    x_tensor =normalize_ultrasonics(x_tensor)
     y_tensor = torch.tensor(y.values, dtype=torch.float32)
-    y_tensor = y_tensor / 100 # 100%を1として正規化
-    y_tensor = steering_shifter_to_01(y_tensor) # -1~1を0~1に変換
+    y_tensor = normalize_motor(y_tensor)
+    y_tensor[:, 0] = steering_shifter_to_01(y_tensor[:, 0])  # ステアリングの値を-1~1を0~1に変換
+    #y_tensor = steering_shifter_to_01(y_tensor) # -1~1を0~1に変換
     print("\nデータ形式の確認:", "x:", x_tensor.shape, "y:", y_tensor.shape)
     return x_tensor, y_tensor, csv_file
+
+def normalize_ultrasonics(x_tensor, scale=2000):
+    x_tensor = x_tensor / scale # 2000mmを1として正規化
+    return x_tensor
+
+def denormalize_ultrasonics(x_tensor, scale=2000):
+    x_tensor = x_tensor * scale # 2000mmを1として正規化
+    return x_tensor
+
+def normalize_motor(y_tensor, scale=100):
+    y_tensor = y_tensor / scale # 100%を1として正規化
+    return y_tensor
+
+def denormalize_motor(y_tensor, scale=100):
+    y_tensor = y_tensor * scale # 100%を1として正規化
+    return y_tensor
 
 # -1~1を0~1に変換
 def steering_shifter_to_01(y_tensor):
@@ -113,12 +130,14 @@ class NeuralNetwork(nn.Module):
 
     # 推論関数
     def predict(self, model, x_tensor):
+        x_tensor = normalize_ultrasonics(x_tensor)
         model.eval()  # モデルを評価モードに設定
         with torch.no_grad():
             predictions = model(x_tensor)
             #predictions = F.softmax(predictions, dim=1)
         predictions = steering_shifter_to_m11(predictions) # 0~1を-1~1に変換
         predictions = torch.clamp(predictions, min=-1, max=1)  # Clamp values between -1 and 1
+        predictions = denormalize_motor(predictions)
         return predictions
 
 # トレーニング関数
